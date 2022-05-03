607a6270a98b2f671a4a02330d33b33cdf184b3e
Techies19 <104831872+Techies19@users.noreply.github.com>
2022-05-03 16:07:00 +0530

    Add files via upload

---
+++ stack_using_array.cpp
@@ -0,0 +1,46 @@
+//stack using array
+class StackUsingArray {
+	int *data;
+	int nextIndex;
+	int capacity;	
+	public :
+	StackUsingArray(int totalSize) {
+		data = new int[totalSize];
+		nextIndex = 0;
+		capacity = totalSize;
+	}
+	// return the number of elements present in my stack
+	int size() {
+		return nextIndex;
+	}
+	bool isEmpty() {
+		return nextIndex == 0;
+	}
+	// insert element
+	void push(int element) {
+		if(nextIndex == capacity) {
+			cout << "Stack full " << endl;
+			return;
+		}
+		data[nextIndex] = element;
+		nextIndex++;
+	}
+	// delete element
+	int pop() {
+		if(isEmpty()) {
+			cout << "Stack is empty " << endl;
+			return INT_MIN;	
+		}
+		nextIndex--;
+		return data[nextIndex];
+	}
+	int top() {
+		if(isEmpty()) {
+			cout << "Stack is empty " << endl;
+			return INT_MIN;	
+		}
+		return data[nextIndex - 1];
+	}
+};
+
+
